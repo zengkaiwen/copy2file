2733bb30bf6ef115cd174874ca323dd96f63d79a
zengkaiwen <zkevin1997@outlook.com>
2023-09-02 20:47:16 +0800

    mod: link

--- clipboardtool/fileOnWindows.go
+++ clipboardtool/fileOnWindows.go
@@ -6,6 +6,7 @@ import (
 )
 
 func winCopy2file(fileName string) error {
+	// 借助开源工具 https://github.com/rostok/file2clip 实现
 	cmd := exec.Command("./file2clip.exe", fileName)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
